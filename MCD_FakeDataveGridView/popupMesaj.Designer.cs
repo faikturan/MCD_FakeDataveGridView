c72c72f9ee934a45c14d659d70307c164dd1aaca
Faik Turan <turanfaik@hotmail.com>
2021-12-10 19:41:22 +0300

    code changed

---
+++ MCD_FakeDataveGridView/popupMesaj.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace MCD_FakeDataveGridView
+{
+    partial class popupMesaj
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtIsim = new System.Windows.Forms.TextBox();
+            this.txtSoyisim = new System.Windows.Forms.TextBox();
+            this.txtTamAd = new System.Windows.Forms.TextBox();
+            this.txtIlIlce = new System.Windows.Forms.TextBox();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.txtEmailAdres = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.btnKapat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(58, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İsim:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(58, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyisim:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(58, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Tamisim:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(58, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 18);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "İl&İlçe";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(59, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 18);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Adres:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(59, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 18);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Telefon:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(58, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 18);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Email Adres:";
+            // 
+            // txtIsim
+            // 
+            this.txtIsim.Location = new System.Drawing.Point(188, 32);
+            this.txtIsim.Name = "txtIsim";
+            this.txtIsim.Size = new System.Drawing.Size(167, 20);
+            this.txtIsim.TabIndex = 2;
+            // 
+            // txtSoyisim
+            // 
+            this.txtSoyisim.Location = new System.Drawing.Point(188, 60);
+            this.txtSoyisim.Name = "txtSoyisim";
+            this.txtSoyisim.Size = new System.Drawing.Size(167, 20);
+            this.txtSoyisim.TabIndex = 2;
+            // 
+            // txtTamAd
+            // 
+            this.txtTamAd.Location = new System.Drawing.Point(188, 86);
+            this.txtTamAd.Name = "txtTamAd";
+            this.txtTamAd.Size = new System.Drawing.Size(167, 20);
+            this.txtTamAd.TabIndex = 2;
+            // 
+            // txtIlIlce
+            // 
+            this.txtIlIlce.Location = new System.Drawing.Point(188, 112);
+            this.txtIlIlce.Name = "txtIlIlce";
+            this.txtIlIlce.Size = new System.Drawing.Size(167, 20);
+            this.txtIlIlce.TabIndex = 2;
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(188, 138);
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(167, 20);
+            this.txtAdres.TabIndex = 2;
+            // 
+            // txtEmailAdres
+            // 
+            this.txtEmailAdres.Location = new System.Drawing.Point(188, 163);
+            this.txtEmailAdres.Name = "txtEmailAdres";
+            this.txtEmailAdres.Size = new System.Drawing.Size(167, 20);
+            this.txtEmailAdres.TabIndex = 2;
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(188, 187);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(167, 20);
+            this.txtTelefon.TabIndex = 2;
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKapat.Location = new System.Drawing.Point(188, 249);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(167, 30);
+            this.btnKapat.TabIndex = 3;
+            this.btnKapat.Text = "Kapat";
+            this.btnKapat.UseVisualStyleBackColor = true;
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
+            // 
+            // popupMesaj
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 323);
+            this.Controls.Add(this.btnKapat);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.txtEmailAdres);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.txtIlIlce);
+            this.Controls.Add(this.txtTamAd);
+            this.Controls.Add(this.txtSoyisim);
+            this.Controls.Add(this.txtIsim);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "popupMesaj";
+            this.Text = "popupMesaj";
+            this.Load += new System.EventHandler(this.popupMesaj_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtIsim;
+        private System.Windows.Forms.TextBox txtSoyisim;
+        private System.Windows.Forms.TextBox txtTamAd;
+        private System.Windows.Forms.TextBox txtIlIlce;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.TextBox txtEmailAdres;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.Button btnKapat;
+    }
+}
---
+++ MCD_FakeDataveGridView/popupMesaj.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCD_FakeDataveGridView
+{
+    public partial class popupMesaj : Form
+    {
+        musteri musteriData;
+        public popupMesaj(musteri data)
+        {
+            InitializeComponent();
+            musteriData = data;
+        }
+
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void popupMesaj_Load(object sender, EventArgs e)
+        {
+            txtIsim.Text = musteriData.isim;
+            txtSoyisim.Text = musteriData.soyisim;
+            txtTamAd.Text = musteriData.tamAdi;
+
+            txtEmailAdres.Text = musteriData.emailAdres;
+            txtTelefon.Text = musteriData.telefonNumara;
+            txtIlIlce.Text = $"{musteriData.il} / {musteriData.ilce}";
+            txtAdres.Text = musteriData.adres;
+        }
+    }
+}
