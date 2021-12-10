d4f3095473aedf91b7dab7ce32817553218467d8
Faik Turan <turanfaik@hotmail.com>
2021-12-10 19:25:47 +0300

    code changed

--- MCD_FakeDataveGridView/Form1.Designer.cs
+++ MCD_FakeDataveGridView/Form1.Designer.cs
@@ -29,21 +29,51 @@ namespace MCD_FakeDataveGridView
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgwMusteriListe = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwMusteriListe)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgwMusteriListe);
+            this.groupBox1.Location = new System.Drawing.Point(12, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(756, 401);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Müşteri Listesi";
+            // 
+            // dgwMusteriListe
+            // 
+            this.dgwMusteriListe.AllowUserToOrderColumns = true;
+            this.dgwMusteriListe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwMusteriListe.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgwMusteriListe.Location = new System.Drawing.Point(3, 16);
+            this.dgwMusteriListe.Name = "dgwMusteriListe";
+            this.dgwMusteriListe.Size = new System.Drawing.Size(750, 382);
+            this.dgwMusteriListe.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwMusteriListe)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgwMusteriListe;
     }
 }
 
